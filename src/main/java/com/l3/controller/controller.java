fbd63bb6d154a2f100a3749d54b2ed97a09d4890
meriembouchenine <boucheninemeriem26@gmail.com>
2026-01-03 21:21:13 +0100

    Refactor controller to Controller with task management
    Refactor controller class to manage tasks and UI.

--- src/main/java/com/l3/controller/controller.java
+++ src/main/java/com/l3/controller/controller.java
@@ -1,10 +1,32 @@
-package com.l3.controller;
+package com.l3.gl;
 
-public class controller {
+import com.l3.gl.model.Task;
+import com.l3.gl.model.TaskManager;
+import com.l3.gl.view.LoginUI;
+import com.l3.gl.view.TaskUI;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Controller {
+    private TaskManager taskManager;
 
-	}
+    public Controller() {
+        taskManager = new TaskManager();
+        new LoginUI(this);
+    }
+    public void showTaskUI() {
+        new TaskUI(this);
+    }
 
+    public void addTask(Task t) {
+        taskManager.addTask(t);
+    }
+    public void removeTask(Task t) {
+        taskManager.removeTask(t);
+    }
+    public List<Task> getTasks() {
+        return taskManager.getTasks();
+    }
+    public static void main(String[] args) {
+        new Controller();
+    }
 }
